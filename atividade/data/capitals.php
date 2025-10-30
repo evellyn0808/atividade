f74c8ccadc9f24af110c2d75dc89944f5766e064
Evellyn <evyy372@gmail.com>
2025-10-30 16:00:17 -0300

    capitals.php

--- atividade/data/capitals.php
+++ atividade/data/capitals.php
@@ -1,18 +1,18 @@
 <?php
 return [
-    ["Walter White", "Breaking Bad"],
-    ["Eleven", "Stranger Things"],
-    ["Daenerys Targaryen", "Game of Thrones"],
-    ["Pam Beesly", "The Office"],
-    ["Rachel Green", "Friends"],
-    ["Geralt de Rívia", "The Witcher"],
-    ["Thomas Shelby", "Peaky Blinders"],
-    ["Din Djarin", "The Mandalorian"],
-    ["Homelander", "The Boys"],
-    ["Loki", "Loki"],
-    ["Joe Goldberg", "You"],
-    ["Sheldon Cooper", "The Big Bang Theory"],
-    ["Ted Lasso", "Ted Lasso"],
-    ["Daryl Dixon", "The Walking Dead"],
-    ["nairóbi", "La Casa de Papel"]
+    ["Michael Scott", "The Office"],
+    ["Rick Grimes", "The Walking Dead"],
+    ["Wanda Maximoff", "WandaVision"],
+    ["Geralt of Rivia", "The Witcher"],
+    ["Jon Snow", "Game of Thrones"],
+    ["Saul Goodman", "Better Call Saul"],
+    ["June Osborne", "The Handmaid's Tale"],
+    ["Dexter Morgan", "Dexter"],
+    ["BoJack Horseman", "BoJack Horseman"],
+    ["Barry Berkman", "Barry"],
+    ["Rue Bennett", "Euphoria"],
+    ["Marty Byrde", "Ozark"],
+    ["Omar Little", "The Wire"],
+    ["Tony Soprano", "The Sopranos"],
+    ["Michael Scofield", "Prison Break"]
 ];
