75706499abb4dc5ee34a86f75d3fe95fcdc824a6
Evellyn <evyy372@gmail.com>
2025-10-30 15:55:00 -0300

    Add files via upload

---
+++ atividade/data/capitals.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    ["Walter White", "Breaking Bad"],
+    ["Eleven", "Stranger Things"],
+    ["Daenerys Targaryen", "Game of Thrones"],
+    ["Pam Beesly", "The Office"],
+    ["Rachel Green", "Friends"],
+    ["Geralt de Rívia", "The Witcher"],
+    ["Thomas Shelby", "Peaky Blinders"],
+    ["Din Djarin", "The Mandalorian"],
+    ["Homelander", "The Boys"],
+    ["Loki", "Loki"],
+    ["Joe Goldberg", "You"],
+    ["Sheldon Cooper", "The Big Bang Theory"],
+    ["Ted Lasso", "Ted Lasso"],
+    ["Daryl Dixon", "The Walking Dead"],
+    ["nairóbi", "La Casa de Papel"]
+];
